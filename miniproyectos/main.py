f8fe8d2263efb661d9b9866e6d6582bad0d4b405
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-01-30 19:25:15 -0500

    freeCodeCamp - 25proy - #3
    -El juego adivina el número (usuario)

---
+++ miniproyectos/main.py
@@ -0,0 +1,4 @@
+# https://www.freecodecamp.org/espanol/news/25-proyectos-en-python-para-principiantes/
+
+if __name__ == '__main__':
+    print('25 Proyectos en Python para principiantes: Ideas para comenzar a programar en Python')
