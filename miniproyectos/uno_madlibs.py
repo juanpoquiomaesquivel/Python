98c02f646ac53eeec92f60aab37d8b048a02b52d
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-01-31 20:34:58 -0500

    freeCodeCamp - 25proy - #1
    -Mad Libs

---
+++ miniproyectos/uno_madlibs.py
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=8ext9G7xspg&t=100s
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous = input("Famous: ")
+
+madlibs = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous}"
+
+print(madlibs)
